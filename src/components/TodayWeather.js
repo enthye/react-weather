645d555eb012215b465e709a74907591f59a5190
enthye <37763349+enthye@users.noreply.github.com>
2019-03-10 23:36:39 +1300

    weather icon implementation

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -7,14 +7,30 @@ class TodayWeather extends React.Component {
                 {this.props.city && this.props.country &&
                 <div>
                     <h1>Today's Weather for {this.props.city}, {this.props.country}.</h1>
-                    <i className={`wi wi-owm-${this.props.conditionsId}`}/>
-                    <p>{this.props.conditions}</p>
-                    <i className="wi wi-thermometer"/>
-                    <p>{this.props.temperature}&#8451;</p>
-                    <i className="wi wi-humidity"/>
-                    <p>{this.props.humidity}%</p>
-                    <i className={`wi wi-wind towards-${this.props.winddirection}-deg`}/>
-                    <p>{this.props.windspeed} km/hr</p>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <i className={`wi wi-owm-${this.props.conditionsId}`}/>
+                                    <p>{this.props.conditions}</p>
+                                </td>
+                                <td>
+                                    <i className="wi wi-thermometer"/>
+                                    <p>{this.props.temperature}&#8451;</p>
+                                </td>
+                                <td>
+                                    <i className="wi wi-humidity"/>
+                                    <p>{this.props.humidity}%</p>
+                                </td>
+                                <td>
+                                    <i className={`wi wi-wind towards-${this.props.winddirection}-deg`}/>
+                                    <p>{this.props.windspeed} km/hr</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
                 </div>}
                 {this.props.error}
             </div>
