48a0cbe3dbbebe615314b393d388044868a79103
enthye <37763349+enthye@users.noreply.github.com>
2019-03-12 22:52:46 +1300

    better alignment of images and texts

--- src/components/ForecastWeather.js
+++ src/components/ForecastWeather.js
@@ -7,31 +7,30 @@ class ForecastWeather extends React.Component {
                 {this.props.forecast &&
                 <div>
                     <h1>5-day Weather Forecast</h1>
-                    <table>
+                    <table className="tableWeather">
                         <thead>
                         <tr>
-
-                            <th className="pCentre" scope="col">1-day</th>
-                            <th className="pCentre" scope="col">2-day</th>
-                            <th className="pCentre" scope="col">3-day</th>
-                            <th className="pCentre" scope="col">4-day</th>
-                            <th className="pCentre" scope="col">5-day</th>
+                            <th className="centre" scope="col">1-day</th>
+                            <th className="centre" scope="col">2-day</th>
+                            <th className="centre" scope="col">3-day</th>
+                            <th className="centre" scope="col">4-day</th>
+                            <th className="centre" scope="col">5-day</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
                             {this.props.forecast.map( (forecast) => (
-                                <td><i className={`pCentre wi wi-owm-${forecast.weather[0].id}`}/></td>
+                                <td align="center"><i className={`wi wi-owm-${forecast.weather[0].id}`}/></td>
                             ))}
                         </tr>
                         <tr>
                             {this.props.forecast.map( (forecast) => (
-                                <td className="pCentre">{forecast.weather[0].description}</td>
+                                <td className="centre">{forecast.weather[0].description}</td>
                             ))}
                         </tr>
                         <tr>
                             {this.props.forecast.map( (forecast) => (
-                                <td className="pCentre">{Math.round(forecast.main.temp)}</td>
+                                <td className="centre">{Math.round(forecast.main.temp)}</td>
                             ))}
                         </tr>
                         </tbody>
--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -7,24 +7,24 @@ class TodayWeather extends React.Component {
                 {this.props.city && this.props.country &&
                 <div>
                     <h1>Today's Weather for {this.props.city}, {this.props.country}.</h1>
-                    <table>
+                    <table className="tableWeather">
                         <tbody>
                             <tr>
-                                <td className="col-md-4">
+                                <td align="center">
                                     <i className={`wi wi-owm-${this.props.conditionsId}`}/>
-                                    <p className="pCentre">{this.props.conditions}</p>
+                                    <p className="centre">{this.props.conditions}</p>
                                 </td>
-                                <td className="col-md-4">
+                                <td align="center">
                                     <i className="wi wi-thermometer"/>
-                                    <p>{this.props.temperature}&#8451;</p>
+                                    <p className="centre">{this.props.temperature}&#8451;</p>
                                 </td>
-                                <td className="col-md-4">
+                                <td align="center">
                                     <i className="wi wi-humidity"/>
-                                    <p>{this.props.humidity}%</p>
+                                    <p className="centre">{this.props.humidity}%</p>
                                 </td>
-                                <td className="col-md-4">
+                                <td align="center">
                                     <i className={`wi wi-wind towards-${this.props.winddirection}-deg`}/>
-                                    <p>{this.props.windspeed} km/hr</p>
+                                    <p className="centre">{this.props.windspeed} km/hr</p>
                                 </td>
                             </tr>
                         </tbody>
